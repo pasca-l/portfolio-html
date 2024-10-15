ed078ee9fa5017a90c115b43b250b453fe7521d2
pasca-l <shion.yamadate@keio.jp>
2024-10-15 23:04:43 +0900

    added effect on home titles

--- js/script.js
+++ js/script.js
@@ -7,4 +7,24 @@ $(document).ready(function () {
 
   // move to the top of the document
   window.scrollTo(0, 0);
+
+  // apply typing effect to home title and subtitle
+  typeWriteEffect("#home-title");
+  typeWriteEffect("#home-subtitle");
 });
+
+function typeWriteEffect(tagId) {
+  var i = 0;
+  const text = $(tagId).text();
+  $(tagId).empty();
+
+  function type() {
+    if (i < text.length) {
+      $(tagId).append(text.charAt(i));
+      i++;
+      setTimeout(type, 70);
+    }
+  }
+
+  type();
+}
